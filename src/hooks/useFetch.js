3f1fa220bd98afbda64ee6b9d1eb2c141809f99d
jennysujukim <jennysujukim@gmail.com>
2023-07-17 00:10:19 -0700

    Update license and README.md

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 
-export const useFetch = (url, isFetch) => {
+export const useFetch = (url) => {
 
     const [data, setData] = useState(null)
     const [isPending, setIsPending] = useState(false)
